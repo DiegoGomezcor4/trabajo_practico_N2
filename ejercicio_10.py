5bc9bcc8e96b1eb54617040d723f015259279d57
Diego Gomez <diegocor4@gmail.com>
2024-05-09 11:33:38 -0300

    ejercicio 10 en proceso

---
+++ ejercicio_10.py
@@ -0,0 +1,22 @@
+"""
+10. Que el usuario ingrese un número entero positivo y muestre por pantalla lo siguiente: 
+a. Todos los números impares desde 1 hasta ese número separados por comas. 
+b. La cuenta atrás desde ese número hasta cero separados por comas. 
+c. Que indique si es primo o no. 
+d. Por último, su factorial
+"""
+
+def numeros_impares():
+    pass
+
+
+def cuenta_atras():
+    pass
+
+
+def es_primo():
+    pass
+
+
+def factorial():
+    pass
