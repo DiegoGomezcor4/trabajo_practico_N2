d82defa3ff5f0676d18badafffa2711ba0fcc243
Diego Gomez <diegocor4@gmail.com>
2024-05-09 12:22:41 -0300

    ejerciocio 10 en proceso

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -6,12 +6,18 @@ c. Que indique si es primo o no.
 d. Por último, su factorial
 """
 
-def numeros_impares():
-    pass
+def numeros_impares(numero):
+    for i in range(1, numero):
+        if (i % 2 != 0):
+            print(i, end=", ")
+    print("")
 
 
-def cuenta_atras():
-    pass
+def cuenta_atras(numero):
+    i = numero
+    while i >= 0:
+        print(i, end=", ")
+        i -= 1
 
 
 def es_primo():
@@ -20,3 +26,15 @@ def es_primo():
 
 def factorial():
     pass
+
+numero = int(input("por favor ingrese un numero entero positivo: "))
+
+print('a. Todos los números impares desde 1 hasta ese número separados por comas: ')
+numeros_impares(numero)
+
+print('b. La cuenta atrás desde ese número hasta cero separados por comas: ')
+cuenta_atras(numero)
+
+print('c. Que indique si es primo o no: ')
+es_primo(numero)
+
