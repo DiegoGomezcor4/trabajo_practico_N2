970699933886e52c06a8f0a3c5a590ec586a5174
Diego Gomez <diegocor4@gmail.com>
2024-05-09 18:45:48 -0300

    ejercicio 11 completo

---
+++ ejercicio_11.py
@@ -0,0 +1,17 @@
+"""
+11. Que solicite al usuario ingresar una frase. Luego, que imprima la cantidad de vocales que se 
+encuentran en dicha frase.
+"""
+
+def contar_vocales(frase):
+    frase = frase.lower()
+    vocales = ['a', 'e', 'i', 'o', 'u']
+    cantidad_vocales = 0
+    for letra in frase:
+        if letra in vocales:
+            cantidad_vocales += 1
+    return cantidad_vocales
+
+frase = input('Por favor ingrese una frase: ')
+print(f'La cantidad de vocales en la frase es: {contar_vocales(frase)}')
+
