5eb1341624fbc6f8e646599f46ad40a498e82f78
Diego Gomez <diegocor4@gmail.com>
2024-05-09 08:53:16 -0300

    ejercicio1

---
+++ ejercicio_1.py
@@ -0,0 +1,11 @@
+"""
+escribir un programa utilizando una función que haga lo siguiente:
+1. Que al pasarle una cadena <nombre> nos muestre por pantalla el saludo ¡Hola <nombre>!.
+"""
+
+def saludo(nombre):
+    print(f'¡Hola {nombre}!')
+
+#prueba:
+saludo("Diego")
+saludo("Javier")
