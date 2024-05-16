cd0f599826800ee093231b7ccae0eec804ad77f3
Diego Gomez <diegocor4@gmail.com>
2024-05-16 10:42:13 -0300

    ejercicio 4 con input

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -11,3 +11,10 @@ def calculo_area(base, altura):
 print(f'El area de un triangulo de base 2 y altura 2 es igual a: {calculo_area(2, 2)}')
 print(f'El area de un triangulo de base 4 y altura 4 es igual a: {calculo_area(4, 4)}')
 print(f'El area de un triangulo de base 6 y altura 9 es igual a: {calculo_area(6, 9)}')
+
+# Ingresado por el usuario:
+
+base_ingresada = float(input('Ingrese la base del triangulo: '))
+altura_ingreada = float(input('Ingrese la altura del triangulo: '))
+area_calculada = calculo_area(base_ingresada, altura_ingreada)
+print(f'El area del triangulo es: {area_calculada}')
