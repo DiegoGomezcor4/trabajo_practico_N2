ff7017f0ca12d02732767847caa67eeef2db2a49
Diego Gomez <diegocor4@gmail.com>
2024-05-16 10:08:29 -0300

    ejercicio3 con input

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -15,5 +15,17 @@ print(f'El total de la factura es: ${total_factura_con_iva(1000, 21)}')
 print(f'El total de la factura es: ${total_factura_con_iva(1000, 10.5)}')
 print(f'El total de la factura es: ${total_factura_con_iva(1000)}')
 
+# Ingresado por el usuario:
+
+cantidad = input('Por favor ingrese el monto de la factura sin iva: ')
+porcentaje = input('Por favor ingrese el porcentaje de iva: ')
+
+if porcentaje == '':
+    resultado = total_factura_con_iva(float(cantidad))
+else:
+    resultado = total_factura_con_iva(float(cantidad), float(porcentaje))
+
+print(f'El total de la factura con iva es igual a: {resultado}')
+
 
 
